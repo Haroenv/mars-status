6e92821cf53ece8ce54a348509c36d23d6f1aebc
Haroen Viaene <hello@haroen.me>
2016-12-08 12:26:43 +0100

    sorting?

--- src/components/MList.vue
+++ src/components/MList.vue
@@ -2,50 +2,134 @@
 <table class="list pa2 mw6 center ba b--light-silver br2 striped--light-gray">
   <thead>
     <th class="pa2">name</th>
-    <th class="pa2">position</th>
+    <th class="pa2">
+      <div class="flex items-center">
+        <span>x</span>
+        <div class="flex flex-column f-1 ma1">
+          <span class="pointer" @click="sort('x', true)">▲</span>
+          <span class="pointer rot180" @click="sort('x', false)">▲</span>
+        </div>
+      </div>
+    </th>
+    <th class="pa2">
+      <div class="flex items-center">
+        <span>y</span>
+        <div class="flex flex-column f-1 ma1">
+          <span class="pointer" @click="sort('y', true)">▲</span>
+          <span class="pointer rot180" @click="sort('y', false)">▲</span>
+        </div>
+      </div>
+    </th>
     <th class="pa2">direction</th>
     <th class="pa2">speed</th>
+    <th class="pa2">
+      <div class="flex items-center">
+        <span>distance</span>
+        <div class="flex flex-column f-1 ma1">
+          <span class="pointer" @click="sort('distance', true)">▲</span>
+          <span class="pointer rot180" @click="sort('distance', false)">▲</span>
+        </div>
+      </div>
+    </th>
   </thead>
   <tbody>
     <tr v-for="rover of rovers" class="b--light-silver bb">
       <td class="pa2"><a :href="`/rover/${rover.id}`">{{rover.name}}</a></td>
-      <td class="pa2">[{{rover.position.x}},{{rover.position.y}}]</td>
-      <td class="pa2">{{rover.direction}}</td>
-      <td class="pa2">{{rover.speed}}</td>
+      <td class="pa2 tc">{{rover.position.x}}</td>
+      <td class="pa2 tc">{{rover.position.y}}</td>
+      <td class="pa2 tc">{{rover.direction}}</td>
+      <td class="pa2 tc">{{rover.speed}}</td>
+      <td class="pa2 tc">{{rover.distance}}</td>
     </tr>
   </tbody>
 </table>
 </template>
 
+<style>
+.rot180 {
+  transform: rotate(180deg);
+}
+.f-1 {
+  font-size: .8em;
+}
+</style>
+
 <script>
 export default {
-  computed: {
-    rovers() {
-      return [{
+  sockets: {
+    list: (val) => {
+      console.log('this method fired by socket server. eg: io.emit("data", data)');
+      console.log(val);
+      // this.rovers = {
+      //   "meme": {
+      //     "name": "ZigZagRover2",
+      //     "position": {
+      //       "x": 21,
+      //       "y": 50
+      //     },
+      //     "direction": 2,
+      //     "speed": 1
+      //   },
+      //   "memey": {
+      //     "name": "ZigZagRover2",
+      //     "position": {
+      //       "x": 21,
+      //       "y": 50
+      //     },
+      //     "direction": 2,
+      //     "speed": 1
+      //   },
+      //   "memer": {
+      //     "name": "ZigZagRover2",
+      //     "position": {
+      //       "x": 21,
+      //       "y": 50
+      //     },
+      //     "direction": 2,
+      //     "speed": 1
+      //   }
+      // };
+    }
+  },
+  data() {
+    return {
+      "rovers": [{
+        "id": "iets",
         "name": "ZigZagRover1",
         "position": {
           "x": 21,
           "y": 50
         },
         "direction": 2,
-        "speed": 1
+        "speed": 1,
+        "distance": 5
       }, {
+        "id": "iets",
         "name": "ZigZagRover1",
         "position": {
           "x": 21,
           "y": 50
         },
         "direction": 2,
-        "speed": 1
+        "speed": 1,
+        "distance": 2
+
       }, {
+        "id": "iets",
         "name": "ZigZagRover1",
         "position": {
           "x": 21,
           "y": 50
         },
         "direction": 2,
-        "speed": 1
-      }];
+        "speed": 1,
+        "distance": 1
+      }]
+    };
+  },
+  methods: {
+    sort: (what, direction) => {
+      console.log(what, direction);
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -6,15 +6,6 @@ Vue.use(VueSocketio, 'http://localhost:8080');
 import App from './App.vue';
 
 const vm = new Vue({
-  sockets: {
-    connect: () => {
-      console.log('socket connected');
-    },
-    list: (val) => {
-      console.log('this method fired by socket server. eg: io.emit("data", data)');
-      console.log(val);
-    }
-  },
   el: '#app',
   render: h => h(App)
 });
