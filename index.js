9f12a0dcce88461a19e457fa2cb86451cb3fc4bf
Elias Meire <eliasmeire.dbz@gmail.com>
2016-12-08 10:47:45 +0100

    socket

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 const got = require('got');
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
 
 app.use(express.static('dist'));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(require('./controllers'));
 
@@ -20,16 +28,16 @@ io.on('connection', function (socket) {
   sendData();
 });
 
-setTimeout(function() {
-  sendData();
-}, 1000 );
-
 sendData = function() {
-	got('http://roguerovers-api-develop.azurewebsites.net/api/channel', {
-		method: 'get'
-	}).then(function(response) {
-		io.emit('list', response);
-	}).catch(function(err) {
-		io.emit('list', {error: "error occured"});
-	});
+	fetch('http://roguerovers-api-develop.azurewebsites.net/api/channel/')
+		.then(function(response) {
+			response.json().then((data) => {
+				io.emit('list', data);
+				setTimeout(sendData, 200);
+			});
+		}).catch(function(err) {
+			io.emit('list', {error: err});
+		});
 };
+
+
