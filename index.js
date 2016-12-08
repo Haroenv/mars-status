3cfc860bfca773e40fe230260a6545a39b7598f8
Elias Meire <eliasmeire.dbz@gmail.com>
2016-12-08 10:03:00 +0100

    list

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const io = require('socket.io')(server);
+
 const port = process.env.PORT || 8080;
 
 const app = express();
+
+const got = require('got');
 
 app.use(express.static('dist'));
 
@@ -11,3 +15,21 @@ app.use(require('./controllers'));
 app.listen(port, function () {
   console.log('Listening on port ' + port);
 });
+
+io.on('connection', function (socket) {
+  sendData();
+});
+
+setTimeout(function() {
+  sendData();
+}, 1000 );
+
+sendData = function() {
+	got('http://roguerovers-api-develop.azurewebsites.net/api/channel', {
+		method: 'get'
+	}).then(function(response) {
+		io.emit('list', response);
+	}).catch(function(err) {
+		io.emit('list', {error: "error occured"});
+	});	
+};
