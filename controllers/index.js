4455ef67a669910bca10af3b4a0db9a4153a9024
Haroen Viaene <hello@haroen.me>
2016-12-08 09:35:14 +0100

    make work

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const router = new express.Router();
 
-router.use('/api/1', require('./api/1'));
-
 router.get('/', function (req, res) {
   res.render('index.pug');
 });
--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 8080;
 
 const app = express();
-
-app.use(bodyParser.json());
 
 app.use(express.static('dist'));
 
