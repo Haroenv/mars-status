6f01371a933d904dd0ab3736c16845b2993ac9f8
Haroen Viaene <hello@haroen.me>
2016-12-08 09:53:17 +0100

    listen to socket.io

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue';
+import VueSocketio from 'vue-socket.io';
+
+Vue.use(VueSocketio, 'http://localhost:8080');
 
 import App from './App.vue';
 
 // eslint-disable-next-line no-new
 new Vue({
+  sockets: {
+    connect: () => {
+      console.log('socket connected');
+    },
+    data: (val) => {
+      console.log('this method fired by socket server. eg: io.emit("data", data)');
+      console.log(val);
+    }
+  },
   el: '#app',
   render: h => h(App)
 });
