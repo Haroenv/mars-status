927553361974463d7a04b8b63438d19bfcd2ad3f
Elias Meire <eliasmeire.dbz@gmail.com>
2016-12-08 12:24:04 +0100

    compose

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-const got = require('got');
 const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
+let workingRovers = [];
+let roversData = [];
 
 app.use(express.static('dist'));
 
@@ -25,19 +26,67 @@ server.listen(port, function () {
 });
 
 io.on('connection', function (socket) {
-  sendData();
+  getWorkingRovers(sendData);
 });
 
-sendData = function() {
-	fetch('http://roguerovers-api-develop.azurewebsites.net/api/channel/')
-		.then(function(response) {
-			return response.json();
-		}).then((data) => {
-      io.emit('list', data);
-      setTimeout(sendData, 200);
+function getWorkingRovers(callback) {
+  console.log("getWorking");
+  fetch('http://roguerovers-api-develop.azurewebsites.net/api/channel/')
+    .then(function(res) {
+      return res.json();
+    }).then((dataRovers) => {
+      workingRovers = [];
+      dataRovers.forEach((rover, i) => {
+        fetch(`http://roguerovers-api-develop.azurewebsites.net/api/channel/${rover}`)
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => {
+            workingRovers.push(rover);
+            if(i === dataRovers.length-2) {
+              callback();
+            }
+          })
+          .catch((err) => {
+          });
+      });
     }).catch(function(err) {
-			io.emit('list', {error: err});
-		});
+      io.emit('list', {error: err});
+    });
+};
+
+function sendData() {
+  console.log("send");
+  roversData = [];
+	workingRovers.forEach((rover, i) => {
+    Promise.all([
+      fetch(`http://roguerovers-api-develop.azurewebsites.net/api/channel/${rover}`),
+      fetch(`http://roguerovers-api-develop.azurewebsites.net/api/channel/${rover}/sensor/w1`),
+      fetch(`http://roguerovers-api-develop.azurewebsites.net/api/channel/${rover}/sensor/t1`)
+    ]).then((values) => {
+      return Promise.all([
+        values[0].json(),
+        values[1].text(),
+        values[2].text()
+      ]);
+    })
+    .then((values) => {
+      let roverObj = values[0];
+      let water = parseFloat(values[1]);
+      let temp = parseFloat(values[2]);
+      roverObj.w1 = water;
+      roverObj.t1 = temp;
+      roverObj.id = rover;
+      roversData.push(roverObj);
+      if(i === workingRovers.length-1) {
+        io.emit('list', roversData);
+        setTimeout(sendData, 200);
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+  });
 };
 
 
