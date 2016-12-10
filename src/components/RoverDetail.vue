ce0b4baeff6351e62b1c505208e6db13c3177be2
Haroen Viaene <hello@haroen.me>
2016-12-10 01:49:31 +0100

    fix border

--- src/components/RoverDetail.vue
+++ src/components/RoverDetail.vue
@@ -1,34 +1,34 @@
 <template>
-<table class="list pa2 mw6 center">
+<table class="list pa2 ma2 center ba">
   <thead>
     <th class="pa2">{{name}}</th>
   </thead>
   <tbody>
-    <tr class="b--light-silver bb">
+    <tr class="bb">
       <td class="pa2">Id</td>
       <td class="pa2 tc">{{id}}</td>
     </tr>
-    <tr class="b--light-silver bb">
+    <tr class="bb">
       <td class="pa2">Position</td>
       <td class="pa2 tc">X: {{position.x}} Y: {{position.y}}</td>
     </tr>
-    <tr class="b--light-silver bb">
+    <tr class="bb">
       <td class="pa2">Speed</td>
       <td class="pa2 tc">{{speed}}</td>
     </tr>
-    <tr class="b--light-silver bb">
+    <tr class="bb">
       <td class="pa2">Distance to base</td>
       <td class="pa2 tc">{{distance}}</td>
     </tr>
-    <tr class="b--light-silver bb">
+    <tr class="bb">
       <td class="pa2">Direction</td>
       <td class="pa2 tc">{{direction}}</td>
     </tr>
-    <tr class="b--light-silver bb">
+    <tr class="bb">
       <td class="pa2">Temperature</td>
       <td class="pa2 tc">{{w1}}</td>
     </tr>
-    <tr class="b--light-silver bb">
+    <tr class="bb">
       <td class="pa2">Water</td>
       <td class="pa2 tc">{{direction}}</td>
     </tr>
